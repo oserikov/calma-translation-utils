2ddae7d2880955ed6c95d6a0a38a0560ae9685e5
Oleg Serikov <srkvoa@gmail.com>
2019-03-01 06:15:19 +0300

    Initial commit

---
+++ test_bilangTranslator.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+from translator import BilangTranslator
+
+
+class TestBilangTranslator(TestCase):
+    l1 = "kaz"
+    l2 = "kir"
+    e_fn1 = "aligned-kaz.vec.short"
+    e_fn2 = "aligned-kir.vec.short"
+    smallTranslator = BilangTranslator(l1, l2, e_fn1, e_fn2)
+    largeTranslator = BilangTranslator(l1, l2, "aligned-kaz.vec", "aligned-kir.vec")
+    # def test__translate_naive(self):
+    #     self.fail()
+    #
+    # def test_translate_naive(self):
+    #     self.fail()
+    #
+    # def test_translate_morfessor_boosted(self):
+    #     self.fail()
+    #
+    def test_get_vector(self):
+        lang = "kir"
+        word = "бир"
+        print(self.smallTranslator.get_vec_by_word(lang, word))
+
+    def test_get_word(self):
+        lang = "kir"
+        word = "бир"
+        assert self.smallTranslator.get_word_by_vec(lang, self.smallTranslator.get_vec_by_word(lang, word)) == word
+
+    def test_set_best_translation_candidates_from_tsv(self):
+        self.smallTranslator.set_best_translation_candidates_from_tsv("kir", "kaz", "kir-kaz-top-translations.tsv")
+        self.smallTranslator.get_best_translation_candidates()
+
+    def test_get_nearest_neighbors(self):
+        lang = "kir"
+        word = "бир"
+        self.smallTranslator.get_nearest_neighbors(lang, self.smallTranslator.get_vec_by_word(lang, word))
+
+
+    def test_meaning_clustering(self):
+
+        lang1 = "kaz"
+        word1 = "құда"
+        lang2 = "kir"
+        word2 = "куда"
+        self.largeTranslator.meaning_clustering(lang1, word1, lang2, word2)
+        word2 = "заттын"
+        self.largeTranslator.meaning_clustering(lang1, word1, lang2, word2)
---
+++ translator.py
@@ -0,0 +1,149 @@
+from gensim.models import KeyedVectors
+import logging
+import sys
+
+
+class BilangTranslator:
+    lang1 = None
+    lang2 = None
+    embeddings_fn1 = None
+    embeddings_fn2 = None
+    models = {}
+
+    best_lang1_to_lang2_translations = []
+
+    logger = logging.getLogger("BilangTranslator")
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('\t'.join(["%(asctime)s", "%(name)s", "%(levelname)s", "%(message)s"]))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    def __init__(self, lang1, lang2, embeddings_fn1, embeddings_fn2):
+        self.lang1 = lang1
+        self.lang2 = lang2
+        self.embeddings_fn1 = embeddings_fn1
+        self.embeddings_fn2 = embeddings_fn2
+        self.logger.info("BEFORE load vectors fn 1")
+        self.models[self.lang1] = KeyedVectors.load_word2vec_format(embeddings_fn1)
+        self.logger.info("AFTER load vectors fn 1")
+        self.logger.info("BEFORE load vectors fn 2")
+        self.models[self.lang2] = KeyedVectors.load_word2vec_format(embeddings_fn2)
+        self.logger.info("AFTER load vectors fn 2")
+
+    def _translate_naive(self, src_lang, tgt_lang, src_word, n=10):
+        assert src_lang in (self.lang1, self.lang2)
+        assert tgt_lang in (self.lang1, self.lang2)
+
+        # src_model = self.models[src_lang]
+        # tgt_model = self.models[tgt_lang]
+
+        src_vec = self.get_vec_by_word(src_lang, src_word)
+        nearest_translations_words = self.get_nearest_neighbors(tgt_lang, src_vec, n=n)
+
+        res = []
+        for translation_candidate_word, distance in nearest_translations_words:
+            res.append((src_word, translation_candidate_word, distance))
+
+        return res
+
+    def translate_naive(self, src_lang, tgt_lang, src_word):
+        return self._translate_naive(src_lang, tgt_lang, src_word)
+
+    # todo implement
+    def translate_morfessor_boosted(self, src_lang, tgt_lang, src_word):
+        naive_translations = self._translate_naive(src_lang, tgt_lang, src_word)
+        raise NotImplementedError()
+
+    def get_vec_by_word(self, lang, word):
+        return self.models[lang][word]
+
+    def get_best_translation_candidates(self):
+        return self.best_lang1_to_lang2_translations
+
+    def set_best_translation_candidates(self, candidates):
+        for (src_lang, src_word), (tgt_lang, tgt_word) in candidates:
+            assert src_lang in (self.lang1, self.lang2)
+            assert tgt_lang in (self.lang1, self.lang2)
+            assert src_lang != tgt_lang
+
+            if src_lang == self.lang1:
+                self.best_lang1_to_lang2_translations.append((src_word, tgt_word))
+            else:
+                # src_lang == self.lang2
+                self.best_lang1_to_lang2_translations.append((tgt_word, src_word))
+
+    def set_best_translation_candidates_from_tsv(self, left_lang, right_lang, tsv_fn):
+        assert left_lang in (self.lang1, self.lang2)
+        assert right_lang in (self.lang1, self.lang2)
+        assert left_lang != right_lang
+
+        translation_candidates_list = []
+        with open(tsv_fn, 'r', encoding="utf-8") as tsv_f:
+            for line in tsv_f:
+                line_splitted = line.rstrip().split('\t')
+                if len(line_splitted) != 2:
+                    self.logger.warning(f"in set_best_translation_candidates_from_tsv(): bad line, skipping: {line}")
+                    continue
+                left_word, right_word = line_splitted[0:2]
+                translation_candidates_list.append(((left_lang, left_word), (right_lang, right_word)))
+
+        self.set_best_translation_candidates(translation_candidates_list)
+
+    def get_word_by_vec(self, lang, vec):
+        return self.models[lang].similar_by_vector(vec, topn=1)[0][0]
+
+    def get_nearest_neighbors(self, lang, vec, n=100):
+        return [(elem[0], elem[1]) for elem in self.models[lang].similar_by_vector(vec, topn=n)]
+
+    def meaning_clustering(self, lang1, word1, lang2, word2):
+
+        # print(self.models[lang1])
+        self.logger.info("BEFORE meaning_clustering()")
+
+        word1_translations = self.translate_default(lang1, lang2, word1)
+        word2_translations = self.translate_default(lang2, lang1, word2)
+        word1_backtranslations = self._get_translations_of_translations(lang2, lang1, word1_translations)
+        word2_backtranslations = self._get_translations_of_translations(lang1, lang2, word2_translations)
+
+        word1_backbacktranslations = self._get_translations_of_translations(lang1, lang2, word1_backtranslations)
+        word2_backbacktranslations = self._get_translations_of_translations(lang2, lang1, word2_backtranslations)
+
+        w1_l2_tr_1 = set([tr[1] for tr in word1_translations])
+        w1_l2_tr_2 = set([tr[1] for tr in word1_backbacktranslations])
+        w1_l1_tr_1 = set([tr[1] for tr in word1_backtranslations])
+
+        w2_l1_tr_1 = set([tr[1] for tr in word2_translations])
+        w2_l1_tr_2 = set([tr[1] for tr in word2_backbacktranslations])
+        w2_l2_tr_1 = set([tr[1] for tr in word2_backtranslations])
+
+        int1 = w1_l2_tr_1.intersection({word2})
+        int2 = w1_l2_tr_1.intersection(w2_l2_tr_1)
+        int3 = w1_l2_tr_2.intersection({word2})
+        int4 = w1_l2_tr_2.intersection(w2_l2_tr_1)
+
+        int5 = w2_l1_tr_1.intersection({word1})
+        int6 = w2_l1_tr_1.intersection(w1_l1_tr_1)
+        int7 = w2_l1_tr_2.intersection({word1})
+        int8 = w2_l1_tr_2.intersection(w1_l1_tr_1)
+
+        sizes = {
+            "depth 0": [
+                {f"{lang1}2{lang2}": len(int1), f"{lang2}2{lang1}": len(int5)},
+                {f"{lang1}2{lang2}": len(int3), f"{lang2}2{lang1}": len(int7)}],
+            "depth 1": {f"{lang1}2{lang2}": len(int2), f"{lang2}2{lang1}": len(int6)},
+            "depth 2": {f"{lang1}2{lang2}": len(int4), f"{lang2}2{lang1}": len(int8)}
+        }
+
+        print(sizes)
+        # print(int1, int2, int3, int4, int5, int6, int7, int8)
+
+        self.logger.info("AFTER meaning_clustering()")
+
+    def _get_translations_of_translations(self, lang1, lang2, word1_translations):
+        backtranslations = [self.translate_default(lang1, lang2, src_w_tr[1]) for src_w_tr in word1_translations]
+        backtranslations = [item for sublist in backtranslations for item in sublist]
+        return backtranslations
+
+    def translate_default(self, src_lang, tgt_lang, src_word):
+        return self._translate_naive(src_lang, tgt_lang, src_word)
